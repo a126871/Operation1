f00c72b7b347833d07c7ff05fc329f6b2f097a01
Megum1 <abtx48692000@gmail.com>
2024-08-29 15:54:34 +0800

    Initial Commit

---
+++ Operation1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Operation1
+//
+//  Created by 何承融 on 2024/8/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
